917aa401e25b76cce047aaa5a2582e1724e3df5d
collerhatguy <jacobshomali3@gmail.com>
2020-07-25 07:09:43 -0400

    template

---
+++ index.js
@@ -0,0 +1,42 @@
+var body = document.getElementsByTagName(body);
+
+// will create program that will listen to user input
+body.addEventListener("keydown", keyPlay(e));
+function keyPlay(e) {
+  //a
+  if ((e.keyCode = 61)) {
+  }
+  //s
+  if ((e.keyCode = 83)) {
+  }
+  //d
+  if ((e.keyCode = 68)) {
+  }
+  //f
+  if ((e.keyCode = 70)) {
+  }
+  //g
+  if ((e.keyCode = 71)) {
+  }
+  //h
+  if ((e.keyCode = 72)) {
+  }
+  //j
+  if ((e.keyCode = 74)) {
+  }
+  //w
+  if ((e.keyCode = 87)) {
+  }
+  //e
+  if ((e.keyCode = 84)) {
+  }
+  //t
+  if ((e.keyCode = 84)) {
+  }
+  //y
+  if ((e.keyCode = 89)) {
+  }
+  //u
+  if ((e.keyCode = 90)) {
+  }
+}
