82a8b09cd7ba5c6f2ced2818f1c72a839be2d8c0
collerhatguy <jacobshomali3@gmail.com>
2020-07-27 08:56:04 -0400

    desperatly trying to fix problems
    i tried to test my keboeard tester so that it returns the right values but instead i get undefined. i also tried to fix the shape of the piano with no results.

--- index.js
+++ index.js
@@ -1,45 +1,64 @@
 var body = document.getElementsByTagName(body);
 
 // will create program that will listen to user input
-body.addEventListener("keydown", keyPlay(e));
-function keyPlay(e) {}
-function whichKey(e) {
-  switch (e.keyCode) {
+window.addEventListener("keypress", keyPlay);
+function keyPlay(e) {
+  const key = whichKey(e);
+  console.log(key);
+}
+function keyStop(e) {
+  const key = whichKey(e);
+}
+function whichKey(a) {
+  switch (a.keyCode) {
     //a
     case 65:
+      console.log("a");
+      return 65;
       break;
     //s
     case 83:
+      return 85;
       break;
     //d
     case 68:
+      return 68;
       break;
     //f
     case 70:
+      return 70;
       break;
     //g
     case 71:
+      return 71;
       break;
     //h
     case 72:
+      return 72;
       break;
     //j
     case 74:
+      return 74;
       break;
     //w
     case 87:
+      return 87;
       break;
     //e
     case 84:
+      return 84;
       break;
     //t
     case 89:
+      return 89;
       break;
     //y
     case 90:
+      return 90;
       break;
     //u
     case 85:
+      return 85;
       break;
     default:
       break;
